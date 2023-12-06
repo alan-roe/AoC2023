c292d426db5e017d7de9c9b7c34234e04f5257ba
Alan Roe <alanroe92@gmail.com>
2023-12-06 20:47:52 +0000

    day6 haskell

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,4 +1,10 @@
 module Main where
+
+import Day1
+import Day2
+import Day3
+import Day4
+import Day6
 import System.Environment (getArgs)
 import Day1 (day1_1, day1_2)
 import Day2 (day2_1, day2_2)
@@ -6,16 +12,22 @@ import Day3 (day3_1, day3_2)
 import Day4 (day4_1, day4_2)
 
 days :: [(String -> String, String -> String)]
-days = [(day1_1, day1_2), (day2_1, day2_2), (day3_1, day3_2), (day4_1, day4_2)] 
+days =
+  [ (day1_1, day1_2),
+    (day2_1, day2_2),
+    (day3_1, day3_2),
+    (day4_1, day4_2),
+    (day6_1, day6_2)
+  ]
 
-day :: String -> IO()
+day :: String -> IO ()
 day d = do
   putStr "Day "
   putStr d
   putStrLn " 2023!"
   s <- readFile ("../assets/day" ++ d ++ ".txt")
-  putStrLn $ (fst $ days !! (read d-1)) s
-  putStrLn $ (snd $ days !! (read d-1)) s
+  putStrLn $ (fst $ days !! (read d - 1)) s
+  putStrLn $ (snd $ days !! (read d - 1)) s
     
 main :: IO ()
 main = do
