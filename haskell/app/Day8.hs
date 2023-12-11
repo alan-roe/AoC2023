b13791a3760c4d050dfb800fe8c3bba4e43a44ed
Alan Roe <alanroe92@gmail.com>
2023-12-11 00:11:16 +0000

    day8 ramhog p2

--- haskell/app/Day8.hs
+++ haskell/app/Day8.hs
@@ -3,6 +3,7 @@ module Day8 where
 import Control.Monad (join)
 import Data.Map (Map)
 import Util (lookup')
+import Control.Parallel.Strategies (parMap, rpar)
 
 type Instructions = [Char]
 
@@ -48,6 +49,17 @@ aToZ' (instruction : ins, nodes) label count = if nextLabel == "ZZZ" then count 
 aToZ :: Network -> Int
 aToZ (instructions, nodes) = aToZ' ((join . repeat) instructions, nodes) "AAA" 0
 
+findStarts :: [Node] -> [Label]
+findStarts nodes = filter (\s -> last s == 'A') (fmap fst nodes)
+
+traverseToZ' :: Network -> (String -> Bool) -> [Label] -> Int -> Int 
+traverseToZ' (instruction : instructions, nodes) f labels count = if all f nextLabels then count+1 else traverseToZ' (instructions, nodes) f nextLabels (count+1)
+  where
+    nextLabels = parMap rpar (\label -> (if instruction == 'L' then fst else snd) (lookup' label nodes)) labels
+
+traverseToZ :: Network -> Int
+traverseToZ (instructions, nodes) = traverseToZ' ((join . repeat) instructions, nodes) (\x -> last x == 'Z') (findStarts nodes) 0
+
 day8_1 = show . aToZ . loadNetwork . lines
 
-day8_2 s = show 0
+day8_2 = show . traverseToZ . loadNetwork . lines
